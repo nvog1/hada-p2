0c93da41175b36e97d43422b59e020e50eccc7d8
nicoslaptopW <nvog1@gcloud.ua.es>
2021-03-10 22:13:24 +0100

    74383864R Jugador.cs: implemented full class structure.

--- hada-p2/Jugador.cs
+++ hada-p2/Jugador.cs
@@ -6,6 +6,60 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
+    class AmonestacionesMaximoExcedidoArgs
+    {
+        private int _amonestaciones;
+        public int amonestaciones
+        {
+            get { return _amonestaciones; }
+            set
+            {
+                _amonestaciones = value;
+            }
+        }
+        
+        public AmonestacionesMaximoExcedidoArgs(int amonestaciones)
+        {
+            this.amonestaciones = amonestaciones;
+        }
+    }
+
+    class FaltasMaximoExcedidoArgs
+    {
+        private int _faltas;
+        public int faltas
+        {
+            get { return _faltas; }
+            set
+            {
+                _faltas = value;
+            }
+        }
+
+        public FaltasMaximoExcedidoArgs(int faltas)
+        {
+            this.faltas = faltas;
+        }
+    }
+
+    class EnergiaMinimaExcedidaArgs
+    {
+        private int _energia;
+        public int energia
+        {
+            get { return _energia; }
+            set
+            {
+                _energia = value;
+            }
+        }
+
+        public EnergiaMinimaExcedidaArgs(int energia)
+        {
+            this.energia = energia;
+        }
+    }
+
     class Jugador
     {
         public static int maxAmonestaciones { get; set; }
@@ -14,33 +68,92 @@ namespace Hada
         public static Random rand { private get; set; }
         public string nombre { get; private set; }
         public int puntos { get; set; }
-        private int amonestaciones 
+
+        private int _amonestaciones;
+        public int amonestaciones
         {
-            get { return amonestaciones; }
-            set 
-            { 
+            get { return _amonestaciones; }
+            set
+            {
                 if (value > maxFaltas) ; //lanzar evento amonestacionesMaximoExcedido
-                else if (value < 0) amonestaciones = 0;
+                else if (value < 0) _amonestaciones = 0;
             }
         }
-        private int faltas 
-        { 
-            get { return faltas; } 
-            set { if (value > maxFaltas) /*lanzar evento faltasMaximoExcedido*/; } 
+
+        private int _faltas;
+        public int faltas
+        {
+            get { return _faltas; }
+            set { if (value > maxFaltas) /*lanzar evento faltasMaximoExcedido*/; }
         }
-        private int energia 
-        { 
-            get { return energia; } 
+
+        private int _energia;
+        public int energia
+        {
+            get { return _energia; }
             set
             {
                 if (value < minEnergia) /*lanzar evento energiaMinimaExcedida*/;
-                if (value > 100) energia = 100;
-                else if (value < 0) energia = 0;
+                if (value > 100) _energia = 100;
+                else if (value < 0) _energia = 0;
             }
-        } 
-
-        static void Main(string[] args)
-        {
         }
+
+        public Jugador(string nombre, int amonestaciones, int faltas, int energia, int puntos)
+        {
+            this.nombre = nombre; this.amonestaciones = amonestaciones; this.faltas = faltas; this.energia = energia; this.puntos = puntos;
+        }
+
+        void incAmonestaciones()
+        {
+
+        }
+
+        void incFaltas()
+        {
+
+        }
+
+        void decEnergia()
+        {
+
+        }
+
+        void incPuntos()
+        {
+
+        }
+
+        bool todoOk()
+        {
+
+        }
+
+        void mover()
+        {
+
+        }
+
+        override
+        string ToString()
+        {
+
+        }
+
+        event EventHandler<AmonestacionesMaximoExcedidoArgs> amonestacionesMaximoExcedido
+        {
+
+        }
+
+        event EventHandler<FaltasMaximoExcedidoArgs> faltasMaximoExcedido
+        {
+
+        }
+
+        event EventHandler<EnergiaMinimaExcedidaArgs> energiaMinimaExcedida
+        {
+
+        }
+
     }
 }
